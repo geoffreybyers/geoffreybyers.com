95e1f5f8f323abfef1b78a67efd053937b74d421
geoffreybyers <geoffreybyers@protonmail.com>
2026-04-16 15:15:01 -0500

    feat: add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://geoffreybyers.com",
   trailingSlash: "never",
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
   markdown: {
     shikiConfig: {
       themes: {
